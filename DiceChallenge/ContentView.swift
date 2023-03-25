2e4339d6b95bef1c166b9532d99c27a176d9cfe9
Danjuma Nasiru <talk2danjumanas@gmail.com>
2023-03-25 17:18:29 +0100

    Initial Commit

---
+++ DiceChallenge/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DiceChallenge
+//
+//  Created by Danjuma Nasiru on 25/03/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DiceChallenge/DiceChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceChallengeApp.swift
+//  DiceChallenge
+//
+//  Created by Danjuma Nasiru on 25/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct DiceChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
